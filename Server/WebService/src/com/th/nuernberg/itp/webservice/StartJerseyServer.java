3a5ea94c33dd514e122c3234b840f2ca7f4ee3c8
baris2887 <AkdagBa45587@ohm-hochschule.de>
2013-04-18 18:03:20 +0200

    Removed JOptionPane.

--- Server/WebService/src/com/th/nuernberg/itp/webservice/StartJerseyServer.java
+++ Server/WebService/src/com/th/nuernberg/itp/webservice/StartJerseyServer.java
@@ -1,6 +1,5 @@
 package com.th.nuernberg.itp.webservice;
 import java.io.IOException;
-//import javax.swing.JOptionPane;
 import com.sun.jersey.api.container.httpserver.HttpServerFactory;
 import com.sun.net.httpserver.HttpServer;
 
@@ -10,7 +9,7 @@ public class StartJerseyServer {
 		server.start();
 		
 		System.out.println("WebService started ...");
-		//JOptionPane.showMessageDialog(null, "Done.");
+
 		//server.stop(0);
 	}
 
