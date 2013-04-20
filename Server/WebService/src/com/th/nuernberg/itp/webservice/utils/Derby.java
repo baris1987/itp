ea5aafb1ce727ca2d7b9036b0b953955d9e27182
baris2887 <AkdagBa45587@ohm-hochschule.de>
2013-04-20 16:42:53 +0200

    Konfiguration in Derby Klasse.

--- Server/WebService/src/com/th/nuernberg/itp/webservice/utils/Derby.java
+++ Server/WebService/src/com/th/nuernberg/itp/webservice/utils/Derby.java
@@ -1,16 +1,25 @@
 package com.th.nuernberg.itp.webservice.utils;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+import com.th.nuernberg.itp.webservice.Configuration;
+import com.th.nuernberg.itp.webservice.Constants;
+
 public class Derby {
-    public static void ConnectionTest() throws ClassNotFoundException, SQLException {
+	// Test Connection Method
+    public static void ConnectionTest() throws ClassNotFoundException, SQLException, IOException {
+    	
+    	Configuration.load(Constants.Configuration);
+    	
         Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
         Properties properties = new Properties();
-        properties.put("user", "itp");
-        properties.put("password", "itp2013");
+        properties.put("user", Configuration.get("Database.Username"));
+        properties.put("password", Configuration.get("Database.Password"));
+        
         Connection connection = DriverManager.getConnection("jdbc:derby:./database;create=true", properties);
         connection.close();
     }
