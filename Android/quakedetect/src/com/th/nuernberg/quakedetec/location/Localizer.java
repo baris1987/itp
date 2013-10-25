0728351b66854700a38e54b4b13526304380ed19
Niklas <schaeferni42225@ohm-hochschule.de>
2013-10-25 08:20:24 +0200

    - Bug fix

--- Android/quakedetect/src/com/th/nuernberg/quakedetec/location/Localizer.java
+++ Android/quakedetect/src/com/th/nuernberg/quakedetec/location/Localizer.java
@@ -1,15 +1,12 @@
 package com.th.nuernberg.quakedetec.location;
 
-import android.app.Service;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
@@ -17,7 +14,7 @@ import com.th.nuernberg.quakedetec.screens.DeviceMap;
 import com.th.nuernberg.quakedetec.screens.Info;
 import com.th.nuernberg.quakedetec.service.NotificationsService;
 
-public class Localizer extends Service implements LocationListener {
+public class Localizer implements LocationListener {
 	private static String TAG = "Localizer";
 	private LocationManager locationManager;
 	private Location locationFromLastSignal;
