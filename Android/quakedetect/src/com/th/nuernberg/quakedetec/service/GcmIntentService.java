fd92abfdb18747642c7b8a243f1fe544dec41ced
Christopher Althaus <althaus.christopher@gmail.com>
2013-11-13 21:04:47 +0100

    GCM Funktionsfähig gemacht

--- Android/quakedetect/src/com/th/nuernberg/quakedetec/service/GcmIntentService.java
+++ Android/quakedetect/src/com/th/nuernberg/quakedetec/service/GcmIntentService.java
@@ -7,11 +7,10 @@ import com.th.nuernberg.quakedetec.receiver.GcmBroadcastReceiver;
 
 import android.app.IntentService;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -52,18 +51,7 @@ public class GcmIntentService extends IntentService {
                 sendNotification("Deleted messages on server: " + extras.toString());
             // If it's a regular GCM message, do some work.
             } else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
-                // This loop represents the service doing some work.
-                for (int i = 0; i < 5; i++) {
-                    Log.i(TAG, "Working... " + (i + 1)
-                            + "/5 @ " + SystemClock.elapsedRealtime());
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                    }
-                }
-                Log.i(TAG, "Completed work @ " + SystemClock.elapsedRealtime());
-                // Post notification of received message.
-                sendNotification("Received: " + extras.toString());
+                sendNotification(extras.getString("message"));
                 Log.i(TAG, "Received: " + extras.toString());
             }
         }
@@ -75,8 +63,7 @@ public class GcmIntentService extends IntentService {
     // This is just one simple example of what you might choose to do with
     // a GCM message.
     private void sendNotification(String msg) {
-        mNotificationManager = (NotificationManager)
-                this.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
 
         //Hier sollten wir eine Activity erstellen, die angezeigt wird wenn man die Nachricht oeffnet
      //   PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
@@ -85,7 +72,9 @@ public class GcmIntentService extends IntentService {
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
         .setSmallIcon(R.drawable.icon)
-        .setContentTitle("GCM Notification")
+        .setVibrate(new long[]{0,300,100,300,100,300,100,300,100,300,100})
+        .setSound(Uri.parse("android.resource://" + this.getPackageName() + "/" + R.raw.alarm))
+        .setContentTitle("QuakeDetec")
         .setStyle(new NotificationCompat.BigTextStyle()
         .bigText(msg))
         .setContentText(msg);
--- Server/WebService/src/com/th/nuernberg/itp/webservice/GoogleCloudMessaging.java
+++ Server/WebService/src/com/th/nuernberg/itp/webservice/GoogleCloudMessaging.java
@@ -25,17 +25,15 @@ public class GoogleCloudMessaging implements IMessaging {
 	
 	public String send() {
 		
-		String rawData = "{\"registration_ids\":[\"APA91bFgHShF18D2GmGaJE_nmNzFWoXuZDnxmuiHInQrjdAupTz6c4Ixi6ZcOmVz3LtOFcngapqYjPkontV6W2e6CTaMZlC5Z3x3HzlnbQROo4ng4Yi67X6FqOBlHPfP2DcKtzKCaIDCcwzekm0FQNy0KQMB4fiBDwnxsONa3sjrHMPDWJRT06w\"],\"data\":{\"message\":\"Test Notification\"}}";
-		String encodedData;
+		String rawData = "{\"registration_ids\":[\"APA91bGG1nTq4ltHc39IC5SNDO4vhYdn83W0pia7_NvlIh1XEFRyBmi_5rPp4e1Xuol1mfhnu5pKlL-NEVDzAEu-I0e1rOqftfCaREL7EQBeJa0y43u3RP5aWqDXEx0ltqnRzHTXNt8smDiSn2VJLF1ScL-e1M7Z0jLWE5uRga0_spKbi4sL7Zo\"],\"data\":{\"message\":\"Test Notification\"}}";
+		 
 		try {
-			encodedData = java.net.URLEncoder.encode( rawData, "UTF-8");
-
 			URL url = new URL("https://android.googleapis.com/gcm/send");
 			HttpURLConnection http = (HttpURLConnection) url.openConnection();
 			http.setDoOutput(true);
 			http.setRequestMethod( "POST" );
 			http.setRequestProperty( "Content-Type", "application/json");
-			http.setRequestProperty( "'Authorization", "key=AIzaSyCQ79pKaeRoWfy-33bcn4oAYZ6ILqWE3H0");
+			http.setRequestProperty( "Authorization", "key=AIzaSyAfSY3J-yW4R2AOdI4UEHpyfFqhSvTFQS8");
 			
 			OutputStreamWriter writer = new OutputStreamWriter( http.getOutputStream() );
 			writer.write( rawData );
