e42efc235f08ce82f4b1a916f388ce47c4eda5bc
Christopher Althaus <althaus.christopher@gmail.com>
2013-11-18 14:32:19 +0100

    Serveradresse auf richtigen Server geändert
    Damit das greift müsst ihr die App komplett deinstallieren, sonst werden
    weiterhin eure alten Einstellungen benutzt

--- Android/quakedetect/src/com/th/nuernberg/quakedetec/service/BackgroundService.java
+++ Android/quakedetect/src/com/th/nuernberg/quakedetec/service/BackgroundService.java
@@ -332,25 +332,10 @@ public class BackgroundService extends Service {
 					}
 					regid = gcm.register(SENDER_ID);
 					msg = "Device registered, registration ID=" + regid;
-
-					// You should send the registration ID to your server over
-					// HTTP, so it
-					// can use GCM/HTTP or CCS to send messages to your app.
 					sendRegistrationIdToBackend();
-
-					// For this demo: we don't need to send it because the
-					// device will send
-					// upstream messages to a server that echo back the message
-					// using the
-					// 'from' address in the message.
-
-					// Persist the regID - no need to register again.
 					storeRegistrationId(context, regid);
 				} catch (IOException ex) {
 					msg = "Error :" + ex.getMessage();
-					// If there is an error, don't just keep trying to register.
-					// Require the user to click a button again, or perform
-					// exponential back-off.
 				}
 				return msg;
 			}
@@ -437,7 +422,7 @@ public class BackgroundService extends Service {
 
 						Log.e(TAG + "_ALARM", "AlarmCount " + String.valueOf(isAlarm) + "/" + String.valueOf(isAlarmCycle) + "->" + String.valueOf(alarmRatio));
 						
-						if (alarmRatio > 15) {
+						if (alarmRatio > 20) {
 							Log.e(TAG + "_ALARM", "EARTHQUAKE!");
 							Toast.makeText(getApplicationContext(), "Earthquake", Toast.LENGTH_SHORT).show();
 							sendAlarmToServer();
